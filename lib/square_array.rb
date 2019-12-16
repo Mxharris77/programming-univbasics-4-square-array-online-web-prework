5c5cf2814f106171518c0bfbad90c7c115b88f7c
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-16 17:30:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  
+  counter = 0
+  numbers = [1,2,3]
+  
+  while numbers[counter]
+    
+     square_array[numbers] = [numbers] * [numbers]
+    counter += 1
+    puts square_array
+   end 
+   
+  
 end
